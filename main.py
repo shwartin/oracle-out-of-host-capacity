6d9a2e6b06e8158bf96d6e71b51be713c3c0ffaf
shwartin <dmtr.mrshn@posteo.net>
2023-05-01 16:12:26 +0400

    fix: refactoring

--- main.py
+++ main.py
@@ -1,37 +1,63 @@
+import os
 import oci
 import time
 import logging
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-config = oci.config.from_file()
+SSH_AUTHORIZED_KEYS = os.getenv("SSH_AUTHORIZED_KEYS")
+IMAGE_ID = os.getenv("IMAGE_ID")
+SUBNET_ID = os.getenv("SUBNET_ID")
+AVAILABILITY_DOMAIN =os.getenv("AVAILABILITY_DOMAIN")
+COMPARTMENT_ID = os.getenv("COMPARTMENT_ID")
+SHAPE = "VM.Standard.A1.Flex"
+SLEEP_TIME = 100
 
-# Enable debug logging
+
+config_from_file = oci.config.from_file()
+oci.base_client.is_http_log_enabled(True)
 logging.getLogger('oci').setLevel(logging.DEBUG)
 
-oci.base_client.is_http_log_enabled(True)
 
-mdata= {"ssh_authorized_keys":"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCotCXpDggwyBkmRZADNRBZjyCnUzup1Xn3xEZ7GRxWDW5j85/JsxXogM15McQHbzS4BUilZ8Uk9ezkID7HAVOs9slpdkE9G2PEuOB+HOz0PqCHyZKH0z/nZJlAwNDfQMVgiOExh0gHS0lWGeYYu8eehz0tI6JM2Gt2wTH9Jwn5tSFWqYF8c+N/wO8yizKQIdnOQzV3FnnaYLKo25/bgo9zE4a+GlrdgZ+96flOWJBECFUUR7tDqy6l602dOlGqAajTaDFVh7KzjKwHJ7yieNpKsP+ayShyWpmjo3L3maenA4BH20W5du3PMWzew7hTjvOBWrdROisTl6fT355aCzN3"}
-shapec = oci.core.models.LaunchInstanceShapeConfigDetails(ocpus=4,memory_in_gbs=24)
-src_details = oci.core.models.InstanceSourceViaBootVolumeDetails(source_type="bootVolume", boot_volume_id="ocid1.image.oc1.ap-singapore-1.aaaaaaaaxe2yk5laktr4a3tpex4augjtr42gfwrfgshyusmer5fcn63m24ua")
+meta_data = {"ssh_authorized_keys": SSH_AUTHORIZED_KEYS}
 
-#{"sourceType":"image","imageId":"ocid1.image.oc1.ap-singapore-1.aaaaaaaaxe2yk5laktr4a3tpex4augjtr42gfwrfgshyusmer5fcn63m24ua","bootVolumeSizeInGBs":50,"bootVolumeVpusPerGB":10}
-src_details2 = oci.core.models.InstanceSourceViaImageDetails(boot_volume_size_in_gbs=50, image_id="ocid1.image.oc1.ap-singapore-1.aaaaaaaaxe2yk5laktr4a3tpex4augjtr42gfwrfgshyusmer5fcn63m24ua", source_type="image")
+shape_config = oci.core.models.LaunchInstanceShapeConfigDetails(
+	ocpus=2,
+	memory_in_gbs=12,
+)
 
-crt_vnicdets = oci.core.models.CreateVnicDetails(assign_private_dns_record=True, assign_public_ip=True, subnet_id = "ocid1.subnet.oc1.ap-singapore-1.aaaaaaaad2ssowg7kg3krdaadlzowu3wqpqn2qbkf3ubrkv43jbxaxesv5ra")
+source_details = oci.core.models.InstanceSourceViaImageDetails(
+	boot_volume_size_in_gbs=50,
+	image_id=IMAGE_ID,
+	source_type="image",
+)
 
-dets = oci.core.models.LaunchInstanceDetails(availability_domain="XZXd:AP-SINGAPORE-1-AD-1",compartment_id="ocid1.tenancy.oc1..aaaaaaaaw5jrkknlr4pyvmaktw2banvrouvx7wz6upllcg4kz2kh5asftn7a",shape="VM.Standard.A1.Flex", shape_config=shapec, metadata=mdata, source_details=src_details2, create_vnic_details=crt_vnicdets)
-#print(dets)
-client = oci.core.ComputeClient(config)
+create_vnic_details = oci.core.models.CreateVnicDetails(
+	assign_private_dns_record=True,
+	assign_public_ip=True,
+	subnet_id=SUBNET_ID,
+)
 
-count = 0
+launch_instance_details = oci.core.models.LaunchInstanceDetails(
+	availability_domain=AVAILABILITY_DOMAIN,
+	compartment_id=COMPARTMENT_ID,
+	shape=SHAPE,
+	shape_config=shape_config,
+	metadata=meta_data,
+	source_details=source_details,
+	create_vnic_details=create_vnic_details
+)
 
-while True:
-	count += 1
-	try:
-		b = client.launch_instance(launch_instance_details=dets)
-		exit(0)
-	except oci.exceptions.ServiceError as err:
-		if (count % 1 == 0):
-			print(err)
-			print("========================================")
-		time.sleep(100)
+if __name__ == "__main__":
+	client = oci.core.ComputeClient(config_from_file)
+
+	while True:
+		try:
+			instance = client.launch_instance(launch_instance_details=launch_instance_details)
+			logging.info("Success! %s", instance)
+			break
+		except oci.exceptions.ServiceError as err:
+			logging.error("Error! %s, waiting %s seconds", err, SLEEP_TIME)
+			time.sleep(SLEEP_TIME)
